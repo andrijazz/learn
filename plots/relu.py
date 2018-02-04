205fd5a058c39e1b3a2b1340b2056592ed90f127
andrijazz <a.djurisic@yahoo.com>
2018-02-04 22:47:26 +0100

    dl week 1 completed

---
+++ plots/relu.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def relu(x):
+    return np.maximum(x, 0)
+
+
+x = np.array([-2, -1, 0, 1, 2, 3])
+y = relu(x, 0)
+plt.plot(x, y)
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Rectified Linear Unit')
+# plt.grid(True)
+plt.savefig("relu.png")
+plt.show()
