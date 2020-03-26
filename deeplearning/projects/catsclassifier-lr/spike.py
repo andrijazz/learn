48dc0e799ff7adec4556551cfaa53b2080020969
andrijazz <a.djurisic@yahoo.com>
2020-03-26 20:40:41 +0100

    refactoring

--- deeplearning/projects/catsclassifier-lr/spike.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import h5py
-
-
-print(np.random.randn(3, 2))
--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-sudo docker run -it --rm -p 8888:8888 -v $PWD:/home/jovyan/work  jupyter/datascience-notebook
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+sudo docker run -it --rm -p 8888:8888 -v $DIR:/home/jovyan/work  jupyter/datascience-notebook
