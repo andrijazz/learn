b1fb61d1e04bded06cdd66a526c3c372f6bf1ef7
andrijazz <a.djurisic@yahoo.com>
2020-04-09 12:41:04 +0200

    dp

--- rl_ua/code/bandits.py
+++ rl_ua/code/bandits.py
@@ -1,68 +1,10 @@
 from rl_glue import RLGlue
-from environment import BaseEnvironment
 from random_agent import RandomAgent
 from greedy_agent import GreedyAgent
+from ten_arm_env import TenArmEnviroment
 import numpy as np
 import tqdm
 import matplotlib.pyplot as plt
-
-
-class TenArmEnviroment(BaseEnvironment):
-    def __init__(self):
-        super(BaseEnvironment, self).__init__()
-        self.arms = None
-        self.seed = None
-
-    def env_init(self, env_info=None):
-        """Setup for the environment called when the experiment first starts.
-
-        Note:
-            Initialize a tuple with the reward, first state observation, boolean
-            indicating if it's terminal.
-        """
-        self.seed = env_info.get("random_seed", None)
-        np.random.seed(self.seed)
-        self.arms = np.random.randn(10)     # [np.random.normal(0.0, 1.0) for _ in range(10)]
-        self.reward_obs_term = (0.0, 0, False)
-
-    def env_start(self):
-        """The first method called when the experiment starts, called before the
-        agent starts.
-
-        Returns:
-            The first state observation from the environment.
-        """
-        s0 = self.reward_obs_term[1]
-        return s0
-
-    def env_step(self, action):
-        """A step taken by the environment.
-
-        Args:
-            action: The action taken by the agent
-
-        Returns:
-            (float, state, Boolean): a tuple of the reward, state observation,
-                and boolean indicating if it's terminal.
-        """
-        sigma = 1.0
-        mu = self.arms[action]
-        reward = sigma * np.random.randn() + mu
-        self.reward_obs_term = (reward, 0, False)
-        return self.reward_obs_term
-
-    def env_cleanup(self):
-        """Cleanup done after the environment ends"""
-
-    def env_message(self, message):
-        """A message asking the environment for information
-
-        Args:
-            message: the message passed to the environment
-
-        Returns:
-            the response (or answer) to the message
-        """
 
 
 def run(agent_class):
---
+++ rl_ua/code/dp.py
@@ -0,0 +1,8 @@
+# Gridworld City, a thriving metropolis with a booming technology industry, has recently experienced an influx of
+# grid-loving software engineers. Unfortunately, the city's street parking system, which charges a fixed rate,
+# is struggling to keep up with the increased demand. To address this, the city council has decided to modify the
+# pricing scheme to better promote social welfare. In general, the city considers social welfare higher when more
+# parking is being used, the exception being that the city prefers that at least one spot is left unoccupied (so that
+# it is available in case someone really needs it). The city council has created a Markov decision process (MDP) to
+# model the demand for parking with a reward function that reflects its preferences. Now the city has hired you — an
+# expert in dynamic programming — to help determine an optimal policy.
