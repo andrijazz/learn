dea35d8ecf352f0f0fcaa4b157f7e96f0a91e480
andrijazz <a.djurisic@yahoo.com>
2021-03-19 23:25:45 +0100

    policy gradients lecture start

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-docker run -d -it --rm -p 8888:8888 -v $DIR:/home/jovyan/work  jupyter/datascience-notebook
+docker run -d -it --rm --name notebook -p 8888:8888 -v $DIR:/home/jovyan/work  jupyter/datascience-notebook
